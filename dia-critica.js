05c588719e41552317b7299638ef49a9165b81a8
Roberto MF <roboe@virgulilla.com>
2017-01-26 01:02:57 +0100

    First commit

---
+++ dia-critica.js
@@ -0,0 +1,88 @@
+// Constants
+const diacriticalAccentWords = [
+  // Monosyllabic
+  'dé',
+  'él',
+  'más',
+  'mí',
+  'sé',
+  'sí',
+  'té',
+  'tú',
+  'tí', // hypercorrection
+
+  // Interrogative, exclamatory, emphatic
+  'qué', // includes 'por qué'
+  'quién',
+  'cómo',
+  'cuál',
+  'cuándo',
+  'cuánta', 'cuántas',
+  'cuánto', 'cuántos',
+  'cuán',
+  'dónde',
+  'adónde',
+
+  // Tonicity
+  'aún',
+
+  // Demostrative pronouns (until 2010)
+  'aquél', 'aquéllos',
+  'aquélla', 'aquéllas',
+  'ésa', 'ésas',
+  'ése', 'ésos',
+
+  // Controversial 'solo' (until 2010... presumably)
+  'sólo',
+
+  // Orthotypographical
+  'ó',
+
+  // Workaround for accents with RegExp insensitivity
+  'Él',
+  'Ésa', 'Ésas',
+  'Ése', 'Ésos',
+  'Ó',
+]
+.map((word) => new RegExp('(^|\W)' + word + '(\W??|$)', 'gi'));
+
+const vowelsMap = {
+  'Á': 'A',
+  'á': 'a',
+  'É': 'E',
+  'é': 'e',
+  'Í': 'I',
+  'í': 'i',
+  'Ó': 'O',
+  'ó': 'o',
+  'Ú': 'U',
+  'ú': 'u',
+}
+
+
+// Functions
+function filterTextNodes(nonEmptyNodes) {
+  return nonEmptyNodes
+    .reduce((textNodes, node) => {
+      [...node.childNodes].forEach((childNode) => {
+        if (childNode.nodeType === 3) textNodes.push(childNode);
+      });
+      return textNodes;
+    }, []);
+}
+
+function removeAccent(word) {
+  return word
+    .replace(/[ÁáÉéÍíÓóÚú]/g, (vowel) => vowelsMap[vowel])
+}
+
+
+// Main
+const nonEmptyNodes = document.querySelectorAll('body:lang(es) *:not(script):not(:empty)');
+const textNodes = filterTextNodes([...nonEmptyNodes]);
+
+diacriticalAccentWords.forEach((word) => {
+  textNodes.forEach((node) => {
+    node.textContent = node.textContent.replace(word, removeAccent);
+  });
+});
